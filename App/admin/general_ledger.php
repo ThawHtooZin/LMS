393b483103d2530609f852f5cb3b1489e7765b3e
Myat Thu <myatthu1582.ygn@gmail.com>
2024-02-15 11:45:49 +0630

    test general ledger

--- App/admin/general_ledger.php
+++ App/admin/general_ledger.php
@@ -259,7 +259,7 @@ $query = new Query();
                   <td><?php if($gldata['debit'] == 0){ echo "-"; }else{if($currencydata['dollar_rate'] == 0){echo $gldata['debit'];}else{ echo $gldata['debit'] / $currencydata['dollar_rate'];  };} ?></td>
                   <td><?php if($gldata['credit'] == 0){ echo "-"; }else{if($currencydata['dollar_rate'] == 0){echo $gldata['credit'];}else{ echo $gldata['credit'] / $currencydata['dollar_rate'];  };} ?></td>
                   <td><?php if($currencydata['dollar_rate'] == 0){ echo "MMK"; }else{ echo "USD"; } ?></td>
-                  <td><?php  if($currencydata['dollar_rate'] == 0){echo $gldata['balance'];}else{ echo $gldata['balance'] / $currencydata['dollar_rate'];  }; ?></td>
+                  <td><?php if($currencydata['dollar_rate'] == 0){echo $gldata['balance'];}else{ echo $gldata['balance'] / $currencydata['dollar_rate'];  }; ?></td>
                   <td>
                     <a href="edittransaction.php?voucher_no=<?= $gldata['voucherno']; ?>&file=general_ledger" style="<?php if(str_contains(strtolower($acname), 'purchase')){ echo "display:none;"; } ?>">
                       <button type="submit" class="btn btn-warning btn-sm text-light" name="updatebutton"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
