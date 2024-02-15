4fb40e2bbcc97e1b862e3f78bb11c6dfba2175d2
Myat Thu <komyatthu1582@gmail.com>
2024-02-15 11:40:32 +0630

    fixed general ledger

--- App/admin/general_ledger.php
+++ App/admin/general_ledger.php
@@ -256,10 +256,10 @@ $query = new Query();
                   <td><?php echo $gldata['voucherno']; ?></td>
                   <td><?php echo $acname; ?></td>
                   <td><?php echo $gldata['narration']; ?></td>
-                  <td><?php if($gldata['debit'] == 0){ echo "-"; }else{if($currencydata['dollar_rate'] == 0){echo $gldata['debit'];}else{ echo $gldata['debit'] / $currencydata['dollar_rate'];  };} ?></td>
-                  <td><?php if($gldata['credit'] == 0){ echo "-"; }else{if($currencydata['dollar_rate'] == 0){echo $gldata['credit'];}else{ echo $gldata['credit'] / $currencydata['dollar_rate'];  };} ?></td>
-                  <td><?php if($currencydata['dollar_rate'] == 0){ echo "MMK"; }else{ echo "USD"; } ?></td>
-                  <td><?php  if($currencydata['dollar_rate'] == 0){echo $gldata['balance'];}else{ echo $gldata['balance'] / $currencydata['dollar_rate'];  }; ?></td>
+                  <td><?php if($gldata['debit'] == 0){ echo "-"; }else{if(!empty($currencydata['dollar_rate']) == 0){echo $gldata['debit'];}else{ echo $gldata['debit'] / $currencydata['dollar_rate'];  };} ?></td>
+                  <td><?php if($gldata['credit'] == 0){ echo "-"; }else{if(!empty($currencydata['dollar_rate']) == 0){echo $gldata['credit'];}else{ echo $gldata['credit'] / $currencydata['dollar_rate'];  };} ?></td>
+                  <td><?php if(!empty($currencydata['dollar_rate']) == 0){ echo "MMK"; }else{ echo "USD"; } ?></td>
+                  <td><?php  if(!empty($currencydata['dollar_rate']) == 0){echo $gldata['balance'];}else{ echo $gldata['balance'] / $currencydata['dollar_rate'];  }; ?></td>
                   <td>
                     <a href="edittransaction.php?voucher_no=<?= $gldata['voucherno']; ?>&file=general_ledger" style="<?php if(str_contains(strtolower($acname), 'purchase')){ echo "display:none;"; } ?>">
                       <button type="submit" class="btn btn-warning btn-sm text-light" name="updatebutton"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
@@ -360,10 +360,10 @@ $query = new Query();
                  <td></td>
                  <td></td>
                  <td></td>
-                 <td><?php if($totaldebit['total_debit'] == 0){ echo "-"; }else{ if($currencydata['dollar_rate'] == 0){echo $totaldebit['total_debit'];}else{ echo $totaldebit['total_debit'] / $currencydata['dollar_rate'];  }; }; ?></td>
-                 <td><?php if($totalcredit['total_credit'] == 0){ echo "-"; }else{ if($currencydata['dollar_rate'] == 0){echo $totalcredit['total_credit'];}else{ echo $totalcredit['total_credit'] / $currencydata['dollar_rate'];  }; }; ?></td>
+                 <td><?php if($totaldebit['total_debit'] == 0){ echo "-"; }else{ if(!empty($currencydata['dollar_rate']) == 0){echo $totaldebit['total_debit'];}else{ echo $totaldebit['total_debit'] / $currencydata['dollar_rate'];  }; }; ?></td>
+                 <td><?php if($totalcredit['total_credit'] == 0){ echo "-"; }else{ if(!empty($currencydata['dollar_rate']) == 0){echo $totalcredit['total_credit'];}else{ echo $totalcredit['total_credit'] / $currencydata['dollar_rate'];  }; }; ?></td>
                  <td></td>
-                 <td><?php if($totalbalance == 0){echo "-"; }else{ if($currencydata['dollar_rate'] == 0){echo $totalbalance;}else{ echo $totalbalance / $currencydata['dollar_rate'];  };} ?></td>
+                 <td><?php if($totalbalance == 0){echo "-"; }else{ if(!empty($currencydata['dollar_rate']) == 0){echo $totalbalance;}else{ echo $totalbalance / $currencydata['dollar_rate'];  };} ?></td>
                  <td></td>
                </tr>
               <?php } } ?>
