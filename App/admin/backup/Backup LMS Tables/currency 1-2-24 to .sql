5730bffbb1c181e33151d31f790b6084f3f78a59
Myat Thu <myatthu1582.ygn@gmail.com>
2024-02-16 10:28:41 +0630

    transfer the tables

---
+++ App/admin/backup/Backup LMS Tables/currency 1-2-24 to .sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2024 at 04:53 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `currency`
+--
+
+CREATE TABLE `currency` (
+  `id` int(11) NOT NULL,
+  `dollar_rate` double NOT NULL,
+  `debitorcredit` varchar(255) NOT NULL,
+  `mmk_amount` bigint(25) NOT NULL,
+  `usd_amount` bigint(25) NOT NULL,
+  `voucher_no` varchar(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `currency`
+--
+
+INSERT INTO `currency` (`id`, `dollar_rate`, `debitorcredit`, `mmk_amount`, `usd_amount`, `voucher_no`) VALUES
+(54, 0, 'debit', 5400, 0, 'Dr-Feb 24/001'),
+(55, 0, 'credit', 5400, 0, 'Dr-Feb 24/001'),
+(58, 0, 'debit', 39000, 0, 'Dr-Feb 24/002'),
+(59, 0, 'credit', 39000, 0, 'Dr-Feb 24/002'),
+(60, 0, 'debit', 14000, 0, 'Dr Feb\'24/003'),
+(61, 0, 'credit', 14000, 0, 'Dr Feb\'24/003'),
+(62, 0, 'debit', 80000, 0, 'Dr Feb\'24/004'),
+(63, 0, 'credit', 80000, 0, 'Dr Feb\'24/004'),
+(64, 0, 'debit', 50000, 0, 'Dr Feb\'24/005'),
+(65, 0, 'credit', 50000, 0, 'Dr Feb\'24/005'),
+(66, 0, 'debit', 50000, 0, 'Dr Feb\'24/006'),
+(67, 0, 'credit', 50000, 0, 'Dr Feb\'24/006'),
+(68, 0, 'debit', 89928700, 0, 'Dr Feb\'24/007'),
+(69, 0, 'credit', 89928700, 0, 'Dr Feb\'24/007'),
+(70, 0, 'debit', 377000, 0, 'Dr Feb\'24/008'),
+(71, 0, 'credit', 377000, 0, 'Dr Feb\'24/008'),
+(72, 0, 'debit', 100000, 0, 'Dr Feb\'24/009'),
+(73, 0, 'credit', 100000, 0, 'Dr Feb\'24/009');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `currency`
+--
+ALTER TABLE `currency`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ App/admin/backup/Backup LMS Tables/transaction 1-2-24 to .sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2024 at 04:47 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `voucher_no` varchar(44) NOT NULL,
+  `ac_code` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `debit` double NOT NULL,
+  `credit` double NOT NULL,
+  `currency` varchar(20) NOT NULL,
+  `sr_no` varchar(25) NOT NULL,
+  `container_no` varchar(25) NOT NULL,
+  `bank_charges` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`id`, `date`, `voucher_no`, `ac_code`, `description`, `debit`, `credit`, `currency`, `sr_no`, `container_no`, `bank_charges`) VALUES
+(54, '2024-02-01', 'Dr-Feb 24/001', '9100/003', 'Water 6 pk purchase for WH', 5400, 0, 'mmk', '', '', 0),
+(55, '2024-02-01', 'Dr-Feb 24/001', '3600/001', 'Water 6 pk purchase for WH', 0, 5400, 'mmk', '', '', 0),
+(58, '2024-02-01', 'Dr-Feb 24/002', '9100/004', 'Sir Phone Bill and Monthly/ Phone 6 pcs Bill/ Sir-6000 (Sir,Daw Nan,Lai-15000) (Office,Sandar,Mar)', 39000, 0, 'mmk', '', '', 0),
+(59, '2024-02-01', 'Dr-Feb 24/002', '3600/001', 'Sir Phone Bill and Monthly/ Phone 6 pcs Bill/ Sir-6000 (Sir,Daw Nan,Lai-15000) (Office,Sandar,Mar)', 0, 39000, 'mmk', '', '', 0),
+(60, '2024-02-01', 'Dr Feb\'24/003', '9100/005', 'Medicine and Taxi for sir', 14000, 0, 'mmk', '', '', 0),
+(61, '2024-02-01', 'Dr Feb\'24/003', '3600/001', 'Medicine and Taxi for sir', 0, 14000, 'mmk', '', '', 0),
+(62, '2024-02-01', 'Dr Feb\'24/004', '9100/006', 'Car Diesel for 5L-2848 (30.1.24-10000)(30.1.24-70000)', 80000, 0, 'mmk', '', '', 0),
+(63, '2024-02-01', 'Dr Feb\'24/004', '3600/001', 'Car Diesel for 5L-2848 (30.1.24-10000)(30.1.24-70000)', 0, 80000, 'mmk', '', '', 0),
+(64, '2024-02-01', 'Dr Feb\'24/005', '9100/006', 'Car Diesel for 6J-3015 (31.1.24-50000)', 50000, 0, 'mmk', '', '', 0),
+(65, '2024-02-01', 'Dr Feb\'24/005', '3600/001', 'Car Diesel for 6J-3015 (31.1.24-50000)', 0, 50000, 'mmk', '', '', 0),
+(66, '2024-02-01', 'Dr Feb\'24/006', '9100/007', 'Material and Taxi Charges for PK Machine', 50000, 0, 'mmk', '', '', 0),
+(67, '2024-02-01', 'Dr Feb\'24/006', '3600/001', 'Material and Taxi Charges for PK Machine', 0, 50000, 'mmk', '', '', 0),
+(68, '2024-02-01', 'Dr Feb\'24/007', '3300/001', 'Bejon Loan take from LM 2020ED Balance $25176*3572ks(Best Brand)', 89928700, 0, 'mmk', '', '', 0),
+(69, '2024-02-01', 'Dr Feb\'24/007', '3600/001', 'Bejon Loan take from LM 2020ED Balance $25176*3572ks(Best Brand)', 0, 89928700, 'mmk', '', '', 0),
+(70, '2024-02-01', 'Dr Feb\'24/008', '9100/008', 'DHL charges for Italy Document sent', 377000, 0, 'mmk', '', '', 0),
+(71, '2024-02-01', 'Dr Feb\'24/008', '3600/001', 'DHL charges for Italy Document sent', 0, 377000, 'mmk', '', '', 0),
+(72, '2024-02-01', 'Dr Feb\'24/009', '4000/K06', 'Paid to Ka Yin Lay Vr No-11337', 100000, 0, 'mmk', '', '', 0),
+(73, '2024-02-01', 'Dr Feb\'24/009', '3600/001', 'Paid to Ka Yin Lay Vr No-11337', 0, 100000, 'mmk', '', '', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
