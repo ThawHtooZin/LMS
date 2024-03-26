f3d3555bd9ea76ec1d299e3caae8091f9ac57e42
Myat Thu <komyatthu1582@gmail.com>
2024-03-26 12:17:00 +0630

    mc report total error fixed

--- App/admin/stockreport.php
+++ App/admin/stockreport.php
@@ -220,7 +220,7 @@ $query = new Query();
               </tr>
               <?php
               }
-              $hhktotalmcstmt = $pdo->prepare("SELECT SUM(mc) AS total_mc FROM hhkmcstock WHERE commondity_id='$item_id' AND particular LIKE '%from%' AND country='$country'");
+              $hhktotalmcstmt = $pdo->prepare("SELECT SUM(mc) AS total_mc FROM hhkmcstock WHERE commondity_id='$item_id' AND particular NOT LIKE '%to%' AND country='$country'");
               $hhktotalmcstmt->execute();
               $hhktotalmcnotsub = $hhktotalmcstmt->fetch(PDO::FETCH_ASSOC);
               $hhktotalmcsubnumstmt = $pdo->prepare("SELECT SUM(mc) AS total_mc FROM hhkmcstock WHERE commondity_id='$item_id' AND particular LIKE '%to%' AND country='$country'");
@@ -238,7 +238,7 @@ $query = new Query();
 
               ?>
               <!-- <tr style="background-color:#c1f5cf;"> -->
-              <tr class="text-center">
+              <tr class="text-center" style="background-color:#c1f5cf;">
                 <td style="font-weight: bold;">Total</td>
                 <td style="font-weight: bold;"></td>
                 <td style="font-weight: bold;"></td>
