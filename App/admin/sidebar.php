dc57895b8e383db74ad919f9b59d8c4e11decd4a
ThawHtooZin <thawhtooin@gmail.com>
2024-07-26 14:57:10 +0630

    added guidebook and tab

--- App/admin/sidebar.php
+++ App/admin/sidebar.php
@@ -562,6 +562,15 @@ $permissions = $permission['permission'];
 
        ?>
       <li class=" p-1" class="nav-item" style=" font-weight:bold; border-bottom: 1px solid rgb(255,255,255,0.2); ">
+        <a href="userg.php" class="nav-link text-light" id="sidebarlink">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+          </svg>
+          <span id="navname">Users Guide</span>
+        </a>
+        </li>
+      <li class=" p-1" class="nav-item" style=" font-weight:bold; border-bottom: 1px solid rgb(255,255,255,0.2); ">
         <a href="logout.php" class="nav-link text-light" id="sidebarlink">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
             <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
---
+++ App/admin/userguide.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include '../../Auth/authrize.ctr.php';
+include '../../Resources/resource.boot.php';
+include '../../Controllers/query.ctr.php';
+
+$auth = new auth();
+$auth->checkadmin();
+$bootstrap = new Bootstrap();
+$query = new Query();
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin | Dashboard</title>
+  </head>
+  <?php
+  $bootstrap->css();
+  ?>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Cormorant+Garamond:wght@300&family=Teko:wght@700&display=swap" rel="stylesheet">
+  <body>
+    <div class="row">
+      <div class="sidebarcol" id="sidebar">
+        <?php
+        include 'sidebar.php';
+        ?>
+      </div>
+      <div class="contentcol" id="content">
+        <?php require 'navbar.php'; ?>
+        <div class="container text-center">
+          <div class="mt-5 ms-5 me-5">
+            <embed src="../../Resources/guidebook.pdf" width="800px" height="635px" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php
+    $bootstrap->javascript();
+    ?>
+  </body>
+</html>
