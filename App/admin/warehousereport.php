0f30741d7d0746b9fc474e53907f71fcd955fdd1
ThawHtooZin <thawhtooin@gmail.com>
2024-09-25 12:02:24 +0630

    added features in warehouse report

--- App/admin/warehousereport.php
+++ App/admin/warehousereport.php
@@ -73,6 +73,63 @@ $query = new Query();
 
 </style>
   <body>
+  <div class="modal" id="filter">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">SELECT REPORT</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="" method="POST">
+                <div class="modal-body">
+                  
+                  <div class="row">
+                    <div class="col">
+                      <label>Start Date:</label>
+                      <input type="date" name="startdate" class="form-control inpv2" value="<?= $_SESSION['startdate']; ?>">
+                    </div>
+                    <div class="col">
+                      <label>End Date:</label>
+                      <input type="date" name="enddate" class="form-control inpv2" value="<?= $_SESSION['enddate']; ?>">
+                    </div>
+                  </div>
+                  
+                  <label>Type of Reports</label>
+                  <select name="filterinp" class="form-control inpv2 mt-2" id="filterinp">
+                    <option value="all" style="font-weight: bold;">All Data</option>
+                    <option value="totalin" style="font-weight: bold;">Total In</option>
+                    <option value="totalout" style="font-weight: bold;">Total Out</option>
+                    <option value="eachmaterialtotalinout-" style="font-weight: bold;">Each Material Total In/Out</option>
+                    <option value="eachmaterialbalance-" style="font-weight: bold;">Each Material Balance</option>
+                    <option value="eachmaterialbalanceamount-" style="font-weight: bold;">Each Material Balance Amount</option>
+                  </select>
+
+                  <label id="packingmateriallabel" style="display: none;">Packing Material</label>
+                  <select name="material" class="form-control inpv2" style="display: none;" id="packingmaterialinp">
+                    <?php
+                      $stmt = $pdo->prepare("SELECT * FROM material_store_house GROUP BY material_id");
+                      $stmt->execute();
+                      $datas = $stmt->fetchAll();
+                      foreach($datas as $data){
+                        $material_id = $data['material_id'];
+                        $stmt = $pdo->prepare("SELECT * FROM materials WHERE id='$material_id'");
+                        $stmt->execute();
+                        $material = $stmt->fetch(PDO::FETCH_ASSOC);
+                        ?>
+                        <option value="<?= $material['id']; ?>" style="padding-left:20px !important;"><?= $material['name']; ?></option>
+                        <?php
+                      }
+                    ?>
+                  </select>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" name="savefilter" class="btn btn-primary">Show</button>
+                </div>
+                </form>
+              </div>
+            </div>
+          </div>
     <div class="row">
       <div class="sidebarcol" id="sidebar">
         <?php
@@ -99,66 +156,10 @@ $query = new Query();
                 }
             ?>
                 <h1 class="merriweather-light text-center">*Packing Material (Link Mark WareHouse) Report*</h1>
+                <h4>Report of Records</h4>
                 <button data-bs-toggle="modal" data-bs-target="#filter" class="ms-2 btn btn-warning float-end " style="font-weight:bold;">Select Report</button>
           </div>
 
-          <div class="modal" id="filter">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title">SELECT REPORT</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form action="" method="POST">
-                <div class="modal-body">
-                  
-                  <div class="row">
-                    <div class="col">
-                      <label>Start Date:</label>
-                      <input type="date" name="startdate" class="form-control inpv2" value="<?= $_SESSION['startdate']; ?>">
-                    </div>
-                    <div class="col">
-                      <label>End Date:</label>
-                      <input type="date" name="enddate" class="form-control inpv2" value="<?= $_SESSION['enddate']; ?>">
-                    </div>
-                  </div>
-                  
-                  <label>Type of Reports</label>
-                  <select name="filterinp" class="form-control inpv2 mt-2" id="filterinp">
-                    <option value="all" style="font-weight: bold;">All Data</option>
-                    <option value="totalin" style="font-weight: bold;">Total In</option>
-                    <option value="totalout" style="font-weight: bold;">Total Out</option>
-                    <option value="eachmaterialtotalinout" style="font-weight: bold;">Each Material Total In/Out</option>
-                    <option value="eachmaterialbalance" style="font-weight: bold;">Each Material Balance</option>
-                    <option value="eachmaterialbalanceamount" style="font-weight: bold;">Each Material Balance Amount</option>
-                  </select>
-
-                  <label id="packingmateriallabel" style="display: none;">Packing Material</label>
-                  <select name="" class="form-control inpv2" style="display: none;" id="packingmaterialinp">
-                    <?php
-                      $stmt = $pdo->prepare("SELECT * FROM material_store_house GROUP BY material_id");
-                      $stmt->execute();
-                      $datas = $stmt->fetchAll();
-                      foreach($datas as $data){
-                        $material_id = $data['material_id'];
-                        $stmt = $pdo->prepare("SELECT * FROM materials WHERE id='$material_id'");
-                        $stmt->execute();
-                        $material = $stmt->fetch(PDO::FETCH_ASSOC);
-                        ?>
-                        <option value="eachmaterialinout-<?= $material['id']; ?>" style="padding-left:20px !important;"><?= $material['name']; ?></option>
-                        <?php
-                      }
-                    ?>
-                  </select>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="submit" name="savefilter" class="btn btn-primary">Show</button>
-                </div>
-                </form>
-              </div>
-            </div>
-          </div>
           <script>
             document.getElementById('filterinp').addEventListener('change', function() {
                 
@@ -248,19 +249,6 @@ $query = new Query();
                     <th>Balance</th>
                   <?php
                 }
-                if(str_contains($filtertype, 'eachmaterialinout-')){
-                  ?>
-                    <th>Id</th>
-                    <th>Date</th>
-                    <th>Voucher No</th>
-                    <th>Supplier</th>
-                    <th>Item Name</th>
-                    <th>Unit</th>
-                    <th>In</th>
-                    <th>Out</th>
-                    <th>Balance</th>
-                  <?php
-                }
                 if(str_contains($filtertype, 'eachmaterialtotalinout-')){
                   ?>
                     <th>Id</th>
@@ -315,6 +303,7 @@ $query = new Query();
               if(isset($_POST['savefilter'])){
                 $_SESSION['startdate'] = $_POST['startdate'];
                 $_SESSION['enddate'] = $_POST['enddate'];
+                $_SESSION['material'] = $_POST['material'];
               }
 
               if(!empty($_SESSION['filtertype'])){
@@ -357,23 +346,8 @@ $query = new Query();
                   $stmt->bindValue(':numOfrecs', $numOfrecs, PDO::PARAM_INT);
                   $stmt->execute();
                 }
-
-                if(str_contains($filtertype, 'eachmaterialinout-')){
-                  $data = explode("-", $filtertype);
-                  $material_id = $data[1];
-                  $stmt = $pdo->prepare("SELECT * FROM material_store_house WHERE material_id = '$material_id' AND `date` BETWEEN '$startdate' AND '$enddate' ORDER BY id");
-                  $stmt->execute();
-                  $rawResult = $stmt->fetchAll();
-                  $total_pages = ceil(count($rawResult) / $numOfrecs);
-
-                  $stmt = $pdo->prepare("SELECT * FROM material_store_house WHERE material_id = '$material_id' AND `date` BETWEEN '$startdate' AND '$enddate' ORDER BY id LIMIT :offset, :numOfrecs");
-                  $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
-                  $stmt->bindValue(':numOfrecs', $numOfrecs, PDO::PARAM_INT);
-                  $stmt->execute();
-                }
                 if(str_contains($filtertype, needle: 'eachmaterialtotalinout-')){
-                  $data = explode("-", $filtertype);
-                  $material_id = $data[1];
+                  $material_id = $_SESSION['material'];
                   $stmt = $pdo->prepare("SELECT * FROM material_store_house WHERE material_id = '$material_id' AND `date` BETWEEN '$startdate' AND '$enddate' GROUP BY material_id ORDER BY id");
                   $stmt->execute();
                   $rawResult = $stmt->fetchAll();
@@ -385,8 +359,7 @@ $query = new Query();
                   $stmt->execute();
                 }
                 if(str_contains($filtertype, needle: 'eachmaterialbalance-')){
-                  $data = explode("-", $filtertype);
-                  $material_id = $data[1];
+                  $material_id = $_SESSION['material'];
                   $stmt = $pdo->prepare("SELECT * FROM material_store_house WHERE material_id = '$material_id' AND `date` BETWEEN '$startdate' AND '$enddate' GROUP BY material_id ORDER BY id");
                   $stmt->execute();
                   $rawResult = $stmt->fetchAll();
@@ -398,8 +371,7 @@ $query = new Query();
                   $stmt->execute();
                 }
                 if(str_contains($filtertype, needle: 'eachmaterialbalanceamount-')){
-                  $data = explode("-", $filtertype);
-                  $material_id = $data[1];
+                  $material_id = $_SESSION['material'];
                   $stmt = $pdo->prepare("SELECT * FROM material_store_house WHERE material_id = '$material_id' AND `date` BETWEEN '$startdate' AND '$enddate' GROUP BY material_id ORDER BY id");
                   $stmt->execute();
                   $rawResult = $stmt->fetchAll();
@@ -459,9 +431,6 @@ $query = new Query();
                   if($filtertype == 'totalout'){
                     $balance += $out;
                   }
-                  if(str_contains($filtertype, 'eachmaterialinout-')){
-                    $balance += $in - $out;
-                  }
                   if(str_contains($filtertype, 'eachmaterialtotalinout-')){
                     $totalinstmt = $pdo->prepare("SELECT SUM(`in`) as totalin FROM material_store_house WHERE material_id='$material_id' AND `date` BETWEEN '$startdate' AND '$enddate'");
                     $totalinstmt->execute();
@@ -499,7 +468,7 @@ $query = new Query();
                     $totalamount = 0;
 
                     foreach($purchasedatas as $purchasedata){
-                      echo $totalamount += $purchasedata['quantity'] * $purchasedata['rate'];
+                      $totalamount += $purchasedata['quantity'] * $purchasedata['rate'];
                     }
 
                     $totalinstmt = $pdo->prepare("SELECT SUM(`in`) as totalin FROM material_store_house WHERE material_id='$material_id' AND `date` BETWEEN '$startdate' AND '$enddate'");
@@ -554,19 +523,6 @@ $query = new Query();
                       <td><?php echo $supplier['supplier_name']; ?></td>
                       <td><?php echo $material['name']; ?></td>
                       <td><?php echo $material['unit']; ?></td>
-                      <td style="color: red; font-weight: bolder;"><?php if($out == ''){echo '-';}else{echo $out;}; ?></td>
-                      <td style="color: blue; font-weight: bolder;"><?php if($balance == ''){echo '-';}else{echo $balance;}; ?></td> 
-                    <?php
-                    }
-                    if(str_contains($filtertype, 'eachmaterialinout-')){
-                      ?>
-                      <td><?php echo $no; ?></td>
-                      <td><?php echo date('d-m-Y', strtotime($data['date'])); ?></td>
-                      <td><?php echo $data['voucher_no']; ?></td>
-                      <td><?php echo $supplier['supplier_name']; ?></td>
-                      <td><?php echo $material['name']; ?></td>
-                      <td><?php echo $material['unit']; ?></td>
-                      <td style="color: red; font-weight: bolder;"><?php if($in == ''){echo '-';}else{echo $in;}; ?></td>
                       <td style="color: red; font-weight: bolder;"><?php if($out == ''){echo '-';}else{echo $out;}; ?></td>
                       <td style="color: blue; font-weight: bolder;"><?php if($balance == ''){echo '-';}else{echo $balance;}; ?></td> 
                     <?php
